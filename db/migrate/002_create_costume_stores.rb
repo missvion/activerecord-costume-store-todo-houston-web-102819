819d0ab9e013706d7d8d61e5035ad661b419bd7e
github username <github email address>
2019-11-09 18:57:14 -0600

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,33 @@
 # Create your costume_stores migration here
+
+
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
+
+
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table (:costume_stores) do |t|
+            t.string (:name)
+            t.string (:location)
+            t.integer (:costume_inventory)
+            t.integer (:num_of_employees)
+            t.boolean (:still_in_business)
+            t.datetime (:opening_time)
+            t.datetime (:closing_time)
+        end
+    end
+
+
+
+end
+
+
+
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,30 @@
 # Create your haunted_houses migration here
+
+# name
+# location
+# theme
+# price
+# whether they're family friendly or not
+# opening date
+# closing date
+# long description
+
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table (:haunted_houses) do |t|
+            t.string (:name)
+            t.string (:location)
+            t.string (:theme)
+            t.float (:price)
+            t.boolean (:family_friendly)
+            t.datetime (:opening_date)
+            t.datetime (:closing_date)
+            t.text (:description)
+        end
+    end
+
+
+
+end
